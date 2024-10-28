529688ccd379e8d874df342969ccb5ce3fb3c736
Muthu Kumar <muthukumarv8985@gmail.com>
2024-10-28 13:53:56 +0530

    completed the destructuring and third commit

--- index.js
+++ index.js
@@ -28,3 +28,33 @@ const ardes="muthukumar";
 // des
 let [m1,m2,m3,m4,m5]=ardes;
 document.write(m2);
+//  array destructuring 
+const fruitdes=["apple","mango","orange","pineapple"]
+let [frut1,frut2,frut3,frut4]= fruitdes;
+document.write(frut4);
+// skiping array values
+const fruitskip=["apple","mango","orange","pineapple","lemon"]
+let [frt1,frt2,,,frt4]=fruitskip;
+document.write(frt4)
+// getting array from index values
+const school =["pen","pencil","scale","eraser"]
+const {[0]:spen ,[1]:spencil}=school;
+document.write(spen)
+//  Destructuring the arrays in map 
+const arrmap=new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+  ]);
+//    destructring
+let detext =" ";
+for (const [key, value] of arrmap) {
+    detext += key + " is " + value;
+  }
+document.write(detext);
+// swapping js variables also in the destructuring
+  let course="React";
+ let duration="three months";
+//  swapping variables
+[course,duration]=[duration,course];
+document.write(course +" "+ duration)
